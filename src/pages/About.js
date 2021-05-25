c083072e5d7cf1cdf2ad0b1898fd872201f60564
ed ben <edwardbenedict92@gmail.com>
2021-05-25 19:16:30 +0300

    feat: add aboutpage

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,31 @@
 import React from "react";
+import aboutSvg from "../assets/work_together.svg";
 
 const About = () => {
   return (
     <div>
-      <h2 style={{ textAlign: "center" }}>About Page</h2>
+      <h1 style={{ textAlign: "center" }}>
+        About Software Developer <span>Ed Ben</span>
+      </h1>
+      <div style={{ textAlign: "center" }}>
+        <img
+          src={aboutSvg}
+          alt="about"
+          style={{ maxWidth: "600px", width: "80%" }}
+        />
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <h2>I'm Ed.</h2>
+        <h4>I’m currently learning Full-Stack Development Languages.</h4>
+        <h4>
+          I've already known JS, ReactJS, ReactNative, NodeJS, MongoDB,SQL,
+          Python, AWS Services.
+        </h4>
+        <h4>
+          <a href="mailto:edwardbenedict92@gmail.com">Send email</a> :
+          edwardbenedict92@gmail.com
+        </h4>
+      </div>
     </div>
   );
 };
