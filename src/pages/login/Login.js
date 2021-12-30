74705f3d8802b84e4a350f87d6d4cada86a78a7b
edwardBenedict <edwardbenedict92@gmail.com>
2021-12-30 15:54:10 +0100

    feat: add login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import MailIcon from '../../assets/icons/Mail';
+import PasswordIcon from '../../assets/icons/Password';
+import style from './Login.module.css'
 
 const Login = () => {
+    const [info, setInfo] = useState({email: '', password: ''})
+
+    const handleChange = (e) => {
+        setInfo({...info, [e.target.name]: e.target.value})
+    }
+
+    const  handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(info)
+    }
     return (
-        <div>
-            <h2>Login</h2>
-           
+        <div className={style.loginWrapper} >
+            <div className={style.login} >
+                <h2>Login</h2>
+                <form style={{display:"flex", flexDirection:"column"}} onSubmit={handleSubmit}>
+                    <label htmlFor="name" style={{}}>Email</label>
+                    <div style={{display:"flex", alignItems:"center"}}>
+                    <MailIcon/>
+                    <input required onChange={handleChange} className={style.input} type="text" id="email" name="email" value={info.email} />
+                    </div>
+                    <label htmlFor="password">Password</label>
+                    <div style={{display:"flex",  alignItems:"center"}}>
+                    <PasswordIcon/>
+                    <input required onChange={handleChange} className={style.input} type="password" id="password" name="password" value={info.password} />
+                    </div>
+                    <button type="submit" className={style.submit}>Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
 
-export default Login
+export default Login;
